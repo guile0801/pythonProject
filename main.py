b3d7439089677212b86cf58dc2389d1d38e21b07
guile0801 <guile0801@gmail.com>
2022-12-05 23:33:53 +0900

    def change

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def print_low(name):
+    print(f'LOW, {name}')
+def print_no(a):
+    print(a +"less")
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +16,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_no('common')
+    print_low('welcome')
     print_hi('PyCharm')
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
