151d98b5dcec58db9e56771082820b64763abe01
guile0801 <guile0801@gmail.com>
2022-12-13 21:05:44 +0900

    def change

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+# 집합
+a = set(['a', 'b', 'c'])
+b = set(['a', 'k', 'l'])
 
+#차집합
+print (a - b)
+#교집합
+print (a & b)
+#합집합
+print (a | b)
 # Press the green button in the gutter to run the script.
-print(__name__)
-print("\n")
-num = int(input())
-print( num * num )
+
 if __name__ == '__main__':
     print_no('common')
     print_low('welcome')
